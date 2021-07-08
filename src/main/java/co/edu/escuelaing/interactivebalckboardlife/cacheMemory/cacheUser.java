557ec5cdc046f39c94abb43e18cb20823a3498a9
Daniel Ducuara <daniel.ducuara@mail.escuelaing.edu.co>
2021-07-08 11:22:06 -0500

    Code

--- src/main/java/co/edu/escuelaing/interactivebalckboardlife/cacheMemory/cacheUser.java
+++ src/main/java/co/edu/escuelaing/interactivebalckboardlife/cacheMemory/cacheUser.java
@@ -1,5 +1,29 @@
 package co.edu.escuelaing.interactivebalckboardlife.cacheMemory;
 
-public class cacheUser {
+import java.util.HashMap;
 
+public class cacheUser {
+	
+	public HashMap<String, String> users = new HashMap<String, String>();
+	private static cacheUser _instance = new cacheUser();
+	/**
+	 * Class constructor SavePoints.
+	 */
+	private cacheUser() {}
+	/**
+	 * this method returns the instance
+	 * @return the class instance
+	 */
+	public static cacheUser getInstance() {
+		return _instance;
+	}
+
+	
+	/**
+	 * users 
+	 * @return the users hashmap
+	 */
+	public HashMap<String, String> getPositions() {
+		return users;
+	}
 }
